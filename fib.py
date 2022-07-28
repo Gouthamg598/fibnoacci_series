2e90ff323ce974dbe365ec6c52cf647868d4e5ea
Goutham <104981580+Gouthamg598@users.noreply.github.com>
2022-07-28 16:04:38 +0530

    Create fib.py

---
+++ fib.py
@@ -0,0 +1,39 @@
+
+# '''fibonacci sequence 0,1,1,2,3,5,8...(0,1,0+1=1,1+1=2,2+1=3,3+2=5......)'''
+# def fib(n):
+#     a=0
+#     b=1
+#     if n<0:
+#         print('negative values are not allowed')
+   
+#     elif n==1:
+#         print(a)
+#     else:
+#             print(a)
+#             print(b)
+#             for i in range(2,n):
+#                 c=a+b#5
+#                 a=b#2
+#                 b=c#3
+#                 print(c)
+# a=int(input('enter a number'))
+# fib(a)
+'''=============================='''
+
+# def fib(t):
+#     a=0
+#     b=1
+#     val=0
+#     if t<0:
+#         print('series is not possible')
+#     else:
+#         while val<=t:
+#             print(val,a,b)
+#             val=a+b
+#             a=b
+#             b=val
+# t=int(input('enter value'))
+# fib(t)
+'''================================'''
+
+            
